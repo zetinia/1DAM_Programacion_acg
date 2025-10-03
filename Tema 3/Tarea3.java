fb3e1004eca6b66741cd3f18f052dc57a531376e
zetinia <acg0072@alu.medac.es>
2025-10-03 09:20:07 +0200

    Create Tarea3.java

---
+++ Tema 3/Tarea3.java
@@ -0,0 +1,15 @@
+public class Tarea3 {
+    public static void main(String[] args) {
+        
+
+        // El y para poner varias codciones es &&
+
+        int numero = Integer.parseInt(System.console().readLine("Que numero de dorsal?: "));
+
+        if(numero==1){
+            System.out.println("Pepe");
+
+        }
+
+    }
+}
