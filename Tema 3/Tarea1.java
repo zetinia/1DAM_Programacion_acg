588d9756bececece488aadcf2c25604053da897d
zetinia <acg0072@alu.medac.es>
2025-10-03 09:20:01 +0200

    Create Tarea1.java

---
+++ Tema 3/Tarea1.java
@@ -0,0 +1,32 @@
+
+//Teañiza un progrma para la entrada de una discoteca, que pregunte la edad e imprima por pantalla si puedes entrar o no en funcion de la edad
+
+public class Tarea1 {
+
+
+    //Mayor que >
+    // menor que < 
+    // mayor o igual que >=
+    //menor o igual que <=
+    // igual ==
+    // desigual !=
+
+   public static void main(String[] args) {
+     int edad = Integer.parseInt (System.console().readLine("Tu edad:"));
+    if (edad >= 18) {
+
+    System.out.println ("Puedes pasar"); 
+
+    } else { 
+        System.out.println("No puedes pasar");
+    }
+  System.out.println("Hasta luego");
+}
+        
+ }
+
+
+
+
+
+   
