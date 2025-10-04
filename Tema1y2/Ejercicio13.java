99ba6a1b5dca754801f254582d28913f6150aace
zetinia <acg0072@alu.medac.es>
2025-10-04 13:06:11 +0200

    Create Ejercicio13.java

---
+++ Tema1y2/Ejercicio13.java
@@ -0,0 +1,22 @@
+//Desarrolla un conversor de bits a Bytes (pide bits, devuelve bytes).
+//
+
+
+public class Ejercicio13 {
+    public static void main(String[] args) {
+        
+
+// Primero creamos la variable que nos permita preguntar cuantos bits queremos pasar a bytes
+
+        int conversor = Integer.parseInt(System.console().readLine("¿Cuantos bits quieres convertir?:"));
+
+//Ahora creamos otra variable donde hacemos la operacion
+
+        float bytes = conversor * 0.125f;
+
+// Por ultimo realizamos el readline para mostrar el cambio
+
+        System.out.println(conversor + " bits  son " +  bytes + " bytes");
+    }
+
+}
