3f5b0d702f8c08bf9d62878c1b396d6ec04e4831
zetinia <acg0072@alu.medac.es>
2025-10-06 13:39:38 +0200

    Create Ejercicio13.java

---
+++ Tema 3/Ejercicio13.java
@@ -0,0 +1,14 @@
+//El programa tiene que calcular la nomina de de construccion 
+//El programa te pregunta si eres albañil, personal de adminstración, comercial o arquitecto. Despues te pide cuantas horas has trabajado esta semana y calcula tu nomina teniendo en cuenta la siguiente tabla semanal:
+
+
+
+public class Ejercicio13 {
+    public static void main(String[] args) {
+        
+
+        
+
+
+    }
+}
