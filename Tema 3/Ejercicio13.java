e551b78870edf43fe113dc2ae0b9051e8211fc12
zetinia <acg0072@alu.medac.es>
2025-10-06 14:12:38 +0200

    Update Ejercicio13.java

--- Tema 3/Ejercicio13.java
+++ Tema 3/Ejercicio13.java
@@ -1,14 +1,52 @@
-//El programa tiene que calcular la nomina de de construccion 
+//El programa tiene que calcular la nomina  de construccion 
 //El programa te pregunta si eres albañil, personal de adminstración, comercial o arquitecto. Despues te pide cuantas horas has trabajado esta semana y calcula tu nomina teniendo en cuenta la siguiente tabla semanal:
-
-
 
 public class Ejercicio13 {
     public static void main(String[] args) {
-        
 
-        
+        String profesion = System.console().readLine("¿Cual es tu cargo?:");
+        int horasTrabajadas = Integer.parseInt(System.console().readLine("¿Cuantas horas has trabajado?"));
+
+        switch (profesion) {
+            case "albañil":
+                if (horasTrabajadas <= 40) {
+                    System.out.println("Ganas " + horasTrabajadas * 15);
+                } else if (horasTrabajadas > 40) {
+                    System.out.println("Has ganado " + horasTrabajadas * 20);
+                }
+
+                break;
+
+            case "administrador":
+                if (horasTrabajadas <= 40) {
+                         System.out.println("Ganas " + horasTrabajadas * 15);
+                } else if (horasTrabajadas > 40) {
+                    System.out.println("Has ganado " + horasTrabajadas * 20);
+                }
+
+                break;
+                
+           case "comercial":
+                if (horasTrabajadas <= 40) {
+                         System.out.println("Ganas " + horasTrabajadas * 15);
+                } else if (horasTrabajadas > 40) {
+                    System.out.println("Has ganado " + horasTrabajadas * 20);
+                }
+
+                break;
+            case "Arquitecto":
+                if (horasTrabajadas <= 40) {
+                         System.out.println("Ganas " + horasTrabajadas * 15);
+                } else if (horasTrabajadas > 40) {
+                    System.out.println("Has ganado " + horasTrabajadas * 20);
+                }
+
+                break;
+                
+
+        }
+
+     }
+ }    
 
 
-    }
-}
