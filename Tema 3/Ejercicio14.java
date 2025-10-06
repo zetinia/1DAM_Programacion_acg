0413d4d685ed40d2cf779a13c04c08fed0209461
zetinia <acg0072@alu.medac.es>
2025-10-06 14:46:08 +0200

    Update Ejercicio14.java

--- Tema 3/Ejercicio14.java
+++ Tema 3/Ejercicio14.java
@@ -1,5 +1,18 @@
+// Crea un menu qie te de a elegir entre si quieres que te felciite tu santo, por tu cumple o por tu aniversario.
+//Si eliges tu santo te preguntara tu nombre y te dira 'feliz San + nombre 
+//Si eliges cumple te pregutnara cauntos años cumples y entonces te felicitara incluyendo tu edad 
+
+
+
+
+
 public class Ejercicio14 {
     public static void main(String[] args) {
         
+System.out.println("Elije una de las siguientes opciones:");
+
+
+
+
     }
 }
