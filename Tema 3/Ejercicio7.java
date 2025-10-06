6b99bfb5a6a739929b535f63c4af6461e4127a35
zetinia <acg0072@alu.medac.es>
2025-10-06 12:55:52 +0200

    Create Ejercicio7.java

---
+++ Tema 3/Ejercicio7.java
@@ -0,0 +1,16 @@
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        int dorsal = Integer.parseInt(System.console().readLine("Introduce un dorsal "));
+
+        switch (dorsal) {
+            case 1:
+                System.out.println("Messi");
+                break;
+        case 2: 
+        System.out.println("CR7");
+                break;
+        }
+    }
+
+    
+}
