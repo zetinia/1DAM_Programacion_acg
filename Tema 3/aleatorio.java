2b180f38c2d699aad838ed3916a08d5f2a9f3b1f
zetinia <acg0072@alu.medac.es>
2025-10-10 09:55:37 +0200

    Create aleatorio.java

---
+++ Tema 3/aleatorio.java
@@ -0,0 +1,17 @@
+public class aleatorio {
+    public static void main(String[] args) {
+   
+
+
+
+     //1) Genera numeros del 0 al 10 ambos incluidos 
+     //2) Geneera numeros del 0 al 9 10 no incluido 
+     //3) Genera numeros del 1 al 10 ambos incluidos
+
+     //4) Genera numeros del 100 al 200 ambos incluidos 
+
+
+
+     int numAleatorio3 = (int) (Math.random() * 101 + 100);
+    }
+}
