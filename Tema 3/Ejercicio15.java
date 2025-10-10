59a265cefb686434718b6107f26c25654d4bf362
zetinia <acg0072@alu.medac.es>
2025-10-10 09:55:40 +0200

    Create Ejercicio15.java

---
+++ Tema 3/Ejercicio15.java
@@ -0,0 +1,8 @@
+public class Ejercicio15 {
+    public static void main(String[] args) {
+        //Desarrolla un programa que, al ejecutarlo, imprima por pantalla aleatoriamente una de las siguientes opciones: piedra,papel o tijeras
+
+
+        
+    }
+}
