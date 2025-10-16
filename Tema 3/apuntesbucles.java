1e5fdb21e5b3deb7b056955ecc2303a300750022
zetinia <acg0072@alu.medac.es>
2025-10-16 13:34:04 +0200

    Create apuntesbucles.java

---
+++ Tema 3/apuntesbucles.java
@@ -0,0 +1,16 @@
+public class apuntesbucles {
+    public static void main(String[] args) {
+        
+
+        //BUCLE FOR -> cantidad concreta y conocida de repeticiones 
+        //Primer hueco : crear la variable i (contador)
+        //Segundo hueco : condicion oara que siga contando 
+        //Tercer hueco: cunadi incremente la variable en cada repet.
+
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println("hola");
+        }
+
+
+    }
+}
