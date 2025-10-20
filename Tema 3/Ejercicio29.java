d053c11efdd5903e945cc11fb2df1573fcbc7fe3
zetinia <acg0072@alu.medac.es>
2025-10-20 14:09:30 +0200

    Create Ejercicio29.java

---
+++ Tema 3/Ejercicio29.java
@@ -0,0 +1,18 @@
+public class Ejercicio29 {
+    public static void main(String[] args) {
+    // bucle for --> repetir un numeri de exacto de veces 
+    //bucle while --> repetir hasta mientras se cumpla una condicion 
+    // while (true) es para un bucle infinito 
+    //DO WHILE --> al menos lo hace una vez 
+
+
+    int num = 10;
+    int usu = Integer.parseInt(System.console().readLine("Num:"));
+
+
+    while (usu < num) {
+        System.out.println("hola");
+        
+    }
+    }
+}
