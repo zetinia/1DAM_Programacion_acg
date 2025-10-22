a27b72372dc0440e6338c002299a06b2646cf2b3
zetinia <acg0072@alu.medac.es>
2025-10-22 18:56:35 +0200

    Create Ejercicio33.java

---
+++ Tema 3/Ejercicio33.java
@@ -0,0 +1,39 @@
+//Crea un programa que simule un saldo bancario. El usuario puede introducir un importe positivo para ingresar dinero y uno negativo para retirar. El programa sigue ejecutandose sea inferior inferior a cero euros.
+
+
+
+
+
+
+
+
+public class Ejercicio33 {
+    public static void main(String[] args) {
+        int cuenta;
+        int retirada;
+        int ingreso;
+
+       
+
+         cuenta = Integer.parseInt(System.console().readLine("¿Cuanto dinero tienes?"));
+         ingreso = Integer.parseInt(System.console().readLine("¿Cuanto quieres ingresar?"));
+        retirada = Integer.parseInt(System.console().readLine("Cuanto deseas retirar?"));
+
+        int menosDinero = cuenta - retirada;
+        int masDinero = cuenta + ingreso;
+        int cuentaTotal = cuenta + masDinero - menosDinero;
+        
+            
+       do {ingreso = Integer.parseInt(System.console().readLine("¿Cuanto quieres ingresar?"));
+         retirada = Integer.parseInt(System.console().readLine("Cuanto deseas retirar?"));
+            System.out.println(cuentaTotal);
+        
+       } while (cuentaTotal <= 0);
+            
+        
+
+
+    }
+  
+}
+ 
