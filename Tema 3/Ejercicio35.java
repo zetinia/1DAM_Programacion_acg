0df31e45acf40c13b9848076a3c83e2834cc7538
zetinia <acg0072@alu.medac.es>
2025-10-23 13:11:26 +0200

    Create Ejercicio35.java

---
+++ Tema 3/Ejercicio35.java
@@ -0,0 +1,14 @@
+public class Ejercicio35 {
+    public static void main(String[] args) {
+        System.out.println("Cuanto dinero quieres ahorrar?");
+        float objetivo = Float.parseFloat(System.console().readLine(""));
+
+        float saldo = 0;
+
+        float ingreso;
+
+        System.out.println("Puedes empezar a meter dinero en la hucha ");
+
+        
+    }
+}
