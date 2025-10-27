179490b6ce8a986cf8de2bdd149bc67e27642acf
zetinia <acg0072@alu.medac.es>
2025-10-27 13:29:18 +0100

    Update Ejercicio2.java

--- Tema 4/Ejercicio2.java
+++ Tema 4/Ejercicio2.java
@@ -8,4 +8,17 @@
 
 public class Ejercicio2 {
     
+public static void main(String[] args) {
+    int numero = Integer.parseInt(System.console().readLine("Num:"));
+
+    repetir(numero);
+}
+
+    public static void repetir(int n){
+        for(int i = 0; i<n;i++){
+            System.out.println("Hola");
+        }
+    }
+
+
 }
