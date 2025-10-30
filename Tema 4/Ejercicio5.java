1a39b8ad28e31c792826c0e6761f5e3d14d6f3a6
zetinia <acg0072@alu.medac.es>
2025-10-30 12:51:56 +0100

    Create Ejercicio5.java

---
+++ Tema 4/Ejercicio5.java
@@ -0,0 +1,38 @@
+/*
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        //Programacion modular -->Crear metodos 
+        String nombre = System.console().readLine("Como te llamas:");
+        String apellido = System.console().readLine("Como te apellidas?");
+        saludar (nombre, apellido);//Lanzar el metodo
+        despedirse(nombre);
+    }
+     
+public static void saludar( String n,String a){
+        System.out.println("hola" + n);
+
+        
+
+    }
+
+    public static void despedirse(String u){
+        System.out.println("Aio + " + u);
+    }
+ 
+        
+
+
+    }
+  
+
+ 
