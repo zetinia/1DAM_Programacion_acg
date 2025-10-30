1e89acb996e47d9616026faf070b28f8392f8333
zetinia <acg0072@alu.medac.es>
2025-10-30 12:51:53 +0100

    Create ejercicio3.java

---
+++ Tema 4/ejercicio3.java
@@ -0,0 +1,29 @@
+/*Multiples metodos con numeros el programa principal (main) pide un numero al usuario y muestra un menu con opciones
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+public class ejercicio3 {
+    public static void main(String[] args) {
+        
+        System.out.println("Elige una de las siguientes opciones:");
+        System.out.println("1) Metodo que imprime si un numero pasado como parametro es par o impar (Imprime la palabra par o impar )");
+        System.out.println("Metodo que imprime directamente la tabla de multiplicar del numero que le pasadmo como parametro ");
+        System.out.println("Metoodo que imprime un numero que sea la mitad del nuemero que le pasamos como parametro ");
+        System.out.println("Metodo que comprueba si es primo e imprime si lo es o no");
+        String opcion = System.console().readLine("¿Cual eliges:");
+
+
+
+        
+
+
+    }
+
+
+}
